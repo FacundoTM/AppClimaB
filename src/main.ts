a5ebdcd52cdfb8e430ce3393cc5b84624d7b1114
Facundo Marchionna <tmfacundo@gmail.com>
2025-05-22 17:01:10 -0300

    Cors actualizados.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://app-clima-f.vercel.app/',
+    origin: 'https://app-clima-f.vercel.app',
     credentials: true,
   });
 
